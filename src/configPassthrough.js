cf93d0eb073a184ee06c2aa56d16386e2e074947
Oliver Caldwell <olliec87@gmail.com>
2016-06-11 17:30:22 +0100

    Clean up config, only build once
    Need to swap to webpack though!

--- src/configPassthrough.js
+++ src/configPassthrough.js
@@ -4,27 +4,29 @@ import _eval from 'eval'
 import babelConfig from './babelConfig'
 import log from './log'
 
-const configFile = 'bastion.conf.js'
+const configFns = readConfigFns('bastion.conf.js')
 
 export default function configPassthrough (name, config) {
-  if (fileExists(configFile)) {
-    log.verbose('found config file')
-
-    const source = babel.transformFileSync(configFile, babelConfig(true)).code
-    const fns = _eval(source, configFile, {}, true)
-
-    if (typeof fns[name] === 'function') {
-      log.verbose('found config function for %s', name)
-
-      return fns[name](config)
-    } else {
-      log.verbose('no config function four %s', name)
-    }
+  if (typeof configFns[name] === 'function') {
+    log.verbose('found config function for %s', name)
+    return configFns[name](config)
   } else {
-    log.verbose('no config file found')
+    log.verbose('no config function four %s', name)
   }
 
   return config
+}
+
+function readConfigFns (configFile) {
+  if (fileExists(configFile)) {
+    log.verbose('found config file %s', configFile)
+    const source = babel.transformFileSync(configFile, babelConfig(true)).code
+    return _eval(source, configFile, {}, true)
+  } else {
+    log.verbose('no config file found at %s', configFile)
+
+    return {}
+  }
 }
 
 function fileExists (path) {
